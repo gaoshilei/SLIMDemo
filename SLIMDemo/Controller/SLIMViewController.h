1a072e780382be016ea16881fd7ce1979375747c
gaoshilei <goslei@163.com>
2018-01-18 11:26:48 +0800

    add Podfile

---
+++ SLIMDemo/Controller/SLIMViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLIMViewController.h
+//  SLIMDemo
+//
+//  Created by gaoshilei on 2018/1/18.
+//  Copyright © 2018年 gaoshilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLIMViewController : UIViewController
+
+@end
---
+++ SLIMDemo/Controller/SLIMViewController.m
@@ -0,0 +1,37 @@
+//
+//  SLIMViewController.m
+//  SLIMDemo
+//
+//  Created by gaoshilei on 2018/1/18.
+//  Copyright © 2018年 gaoshilei. All rights reserved.
+//
+
+#import "SLIMViewController.h"
+
+@interface SLIMViewController ()
+
+@end
+
+@implementation SLIMViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
