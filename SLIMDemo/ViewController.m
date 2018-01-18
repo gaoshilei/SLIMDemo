9a79d98f0f4f6737f1b852d1d65436764dee5f18
gaoshilei <goslei@163.com>
2018-01-18 10:23:31 +0800

    Initial Commit

---
+++ SLIMDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SLIMDemo
+//
+//  Created by gaoshilei on 2018/1/18.
+//  Copyright © 2018年 gaoshilei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
