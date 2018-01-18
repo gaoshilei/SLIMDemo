895586c86192ec78e71114305fe9c6a1c2b33985
gaoshilei <goslei@163.com>
2018-01-18 11:35:22 +0800

    update

--- SLIMDemo/ViewController.m
+++ SLIMDemo/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "SLIMViewController.h"
+#import <Masonry.h>
 
 @interface ViewController ()
 
@@ -16,13 +18,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    self.title = @"客服入口";
+    UIButton *launchBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [launchBtn setTitle:@"客服入口" forState:UIControlStateNormal];
+    [launchBtn setBackgroundColor:[UIColor yellowColor]];
+    [launchBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    launchBtn.layer.cornerRadius = 8.f;
+    launchBtn.layer.masksToBounds = YES;
+    [launchBtn addTarget:self action:@selector(p_customService) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:launchBtn];
+    [launchBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(150.f, 50.f));
+        make.centerX.equalTo(self.view);
+        make.top.equalTo(self.view).offset(150.f);
+    }];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)p_customService {
+    SLIMViewController *IM = [[SLIMViewController alloc] init];
+    [self.navigationController pushViewController:IM animated:YES];
 }
 
 
